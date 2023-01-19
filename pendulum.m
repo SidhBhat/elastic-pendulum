044ea1beb142eec4e34db08cedba9e2d785adb45
Siddharth Bhat <essdeehb@gmail.com>
2023-01-19 18:23:27 +0530

    Major Changes!
    Initial behaviour was to animate while simulating,
    changed this to calculate data first and than plot animation along with
    data.
    
    also now we collect only required number of data points unlike before.

--- pendulum.m
+++ pendulum.m
@@ -1,7 +1,4 @@
-clear all;
-close all;
-
-#### Define Environment ####
+### Define Environment ####
 global k = 10;  # spring constant
 global g = 10;  # gravitatonal acceleration
 global m = 1;   # mass of bob
@@ -11,15 +8,17 @@ global l0        = 0.5;  # L0 the extention when m = 0
 global theta_ini = 0.4;  # initial displacement of pendulum
 
 ## Simulation Control ##
-global t_delta = 0.001;  # accuracy (simulation in steps of delta t)
-T_max   = 30;    # number of units of time to simulate
+global t_delta = 0.00001;  # accuracy (simulation in steps of delta t)
+T_max   = 100;    # number of units of time to simulate
 
 ## plot controls ##
-lim = 1.5;  # the lim of the box containing the pendulum
-FPS = 20; # frames per unit time
-plot_l_theta = 1; # set to 1 if you want to plot l vs theta and related data
+## Important note: this also affects the nuber of data points
+## and thus also the "smoothness" of the plots
+lim  = 1.5;  # the lim of the box containing the pendulum
+FPS  = 20;   # frames per unit time
+anim = 1;    # set to 1 if you want to animate pendulum
 
-###------------------------- begin script -------------------------###
+###------------------------- begin script -------------------------##
 # pendulum structure
 global pendulum;
 
@@ -63,16 +62,53 @@ function update_pendulum ()
 	update_acceleration();
 endfunction
 
+# counter for data collection
+n   = uint64(1);
+# counter for data spacing
+cnt = uint64(0);
+
+## space data accornging to number of frames required
 # frames per unit
 fp_unit = FPS;
 # number of t_deltas between frames
-tp = 1 / fp_unit / t_delta;
-# counter for plot
-cnt = 0;
-# counter for data collection
-n   = 1;
+tp = uint64(1 / fp_unit / t_delta);
 
-## Set up plot
+if(tp > T_max / t_delta)
+	printf("Error: tp cannot be greater than total simpulation points\n");
+	return;
+endif
+
+# arrays to collect data
+L_data(n)     = 0;
+Theta_data(n) = 0;
+t_data        = 0;
+
+#initialise
+initialise_pendulum();
+#measure calculation time
+tstart = clock();
+# simulate/calculate
+for t = 0:t_delta:T_max;
+
+	if(!mod(cnt,tp))
+		## collect data
+		L_data(n)     = pendulum.l;
+		Theta_data(n) = pendulum.theta;
+		t_data(n)     = t;
+		n++;
+		cnt = uint64(1);
+		printf("t = %0.2f\r",t);
+	endif
+	cnt++;
+
+	update_pendulum();
+endfor
+disp("");
+# print total execution time
+exe_time = etime(clock(), tstart);
+printf("Simulation Time\t= %f\n",exe_time);
+
+## setup first figure
 figure(1);
 clf;
 subplot(1,2,1);
@@ -80,74 +116,63 @@ cla;
 subplot(1,2,2);
 cla;
 
-# arrays to collect data
-L_data(n)     = 0;
-Theta_data(n) = 0;
-t_data        = 0:t_delta:T_max;;
+# plot 1 (l vs theta)
+subplot(1,2,1);
+plot(Theta_data, L_data,"-r" , "linewidth", 1);
+axis([-theta_ini, theta_ini, min(L_data), max(L_data)]);
+grid("on");
+title("L vs theta");
+xlabel("theta");
+ylabel("L");
+# plot 2 (l and theta vs t)
+subplot(1,2,2);
+hold "on";
+plot(t_data, Theta_data, "-b", "linewidth", 1);
+plot(t_data, L_data, "-r" , "linewidth", 1);
+legend("theta","L");
+axis([0, T_max, min(Theta_data), max(L_data)]);
+grid("on");
+title("L and theta vs t");
+xlabel("t");
 
-#initialise
-initialise_pendulum();
-#measure execution time
-tstart2 = tstart = clock();
-for t = t_data;
-
-	if(!mod(cnt,tp))
-		# plot 1 (visualisation of pendulum)
-		subplot(1,2,1);
-		h = quiver(0,0, pendulum.l * sin(pendulum.theta), -pendulum.l * cos(pendulum.theta), "k", "linewidth", 1);
-		set(h, "maxheadsize", 0.0);
-		axis([-lim, lim, -2*lim, 0], "equal");
-		grid("on");
-		title(sprintf("t = %0.2f",t));
-		# plot 2 (trajectory equation)
-		subplot(1,2,2);
-		hold "on";
-		plot(pendulum.l * sin(pendulum.theta) ,-pendulum.l * cos(pendulum.theta), "*r", "markersize", 3);
-		axis([-lim, lim, -2*lim, 0], "equal");
-		grid("on");
-		title(sprintf("t = %0.2f",t));
-		## subract execution time
-		exe_time = etime(clock(), tstart);
-		pause(t_delta * tp - exe_time);
-	endif
-	cnt++;
-	## collect data
-	if(plot_l_theta)
-		L_data(n)     = pendulum.l;
-		Theta_data(n) = pendulum.theta;
-		n++;
-	endif
-
-	update_pendulum();
-endfor
-# print total execution time
-exe_time = etime(clock(), tstart2);
-printf("t = %f\n",exe_time);
-
-if(plot_l_theta)
-	## setup second figure
+if (anim)
+	## Set up plot
 	figure(2);
 	clf;
 	subplot(1,2,1);
 	cla;
 	subplot(1,2,2);
 	cla;
-	# plot 1 (l vs theta)
-	subplot(1,2,1);
-	plot(Theta_data, L_data,"-r" , "linewidth", 1);
-	axis([-theta_ini, theta_ini, min(L_data), max(L_data)]);
-	grid("on");
-	title("L vs theta");
-	xlabel("theta");
-	ylabel("L");
-	#plot 2 (l and theta vs t)
-	subplot(1,2,2);
-	hold "on";
-	plot(t_data, Theta_data, "-b", "linewidth", 1);
-	plot(t_data, L_data, "-r" , "linewidth", 1);
-	legend("theta","L");
-	axis([0, T_max, min(Theta_data), max(L_data)]);
-	grid("on");
-	title("L and theta vs t");
-	xlabel("t");
+
+	X_data = L_data .* sin(Theta_data);
+	Y_data = - L_data .* cos(Theta_data);
+
+	# Measure animation time
+	tstart2 = clock();
+	#start Animation
+	for m = 1:--n;
+		tstart = clock();
+
+		# plot 1 (visualisation of pendulum)
+		subplot(1,2,1);
+		h = quiver(0,0, X_data(m), Y_data(m), "k", "linewidth", 1);
+		set(h, "maxheadsize", 0.0);
+		axis([-lim, lim, -2*lim, 0], "equal");
+		grid("on");
+		title(sprintf("t = %0.2f",(m)*t_delta));
+		# plot 2 (trajectory plot)
+		subplot(1,2,2);
+		hold "on";
+		plot(X_data(m), Y_data(m), "*r", "markersize", 3);
+		axis([-lim, lim, -2*lim, 0], "equal");
+		grid("on");
+		title(sprintf("t = %0.2f",(m)*t_delta));
+
+		## subract execution time
+		exe_time = etime(clock(), tstart);
+		pause(t_delta * tp - exe_time);
+	endfor
+	# print total animation time
+	exe_time = etime(clock(), tstart2);
+	printf("Animation Time\t= %f\n",exe_time);
 endif
